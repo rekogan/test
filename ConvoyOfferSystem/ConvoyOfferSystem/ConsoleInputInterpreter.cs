0f90c178ba43e912576536dc27919b18a9551cfb
rekogan <rekogan@hotmail.com>
2016-03-31 15:03:36 -0700

    test version 1

--- ConvoyOfferSystem/ConvoyOfferSystem/ConsoleInputInterpreter.cs
+++ ConvoyOfferSystem/ConvoyOfferSystem/ConsoleInputInterpreter.cs
@@ -12,44 +12,69 @@ namespace ConvoyOfferSystem
 
         public static bool ProcessCommand(string line, IOfferSystem offerSystem)
         {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                Console.WriteLine("Error: Valid command is missing");
+            }
             var parts = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
             string command = parts[0].ToLowerInvariant();
             switch (command)
             {
                 case "driver":
+                    if (parts.Length < 3)
+                    {
+                        OutputInsufficientArguments(parts[0]);
+                    }
+
                     int capacity;
                     if (!int.TryParse(parts[1], out capacity))
                     {
                         OutputInvalidArgument(command, parts[1]);
+                        return true;
                     }
 
                     offerSystem.Driver(GetDriverName(parts, 2), capacity);
                     break;
 
                 case "shipment":
+                    if (parts.Length < 3)
+                    {
+                        OutputInsufficientArguments(parts[0]);
+                    }
+
                     int shipmentId;
                     if (!int.TryParse(parts[1], out shipmentId))
                     {
                         OutputInvalidArgument(command, parts[1]);
+                        return true;
                     }
                     int shipCapacity;
                     if (!int.TryParse(parts[2], out shipCapacity))
                     {
                         OutputInvalidArgument(command, parts[2]);
+                        return true;
                     }
 
                     offerSystem.Shipment(shipmentId, shipCapacity);
                     break;
 
                 case "offer":
+                    if (parts.Length < 4)
+                    {
+                        OutputInsufficientArguments(parts[0]);
+                    }
+
                     if (!int.TryParse(parts[1], out shipmentId))
                     {
                         OutputInvalidArgument(command, parts[1]);
+                        return true;
                     }
                     OfferResult offerResult;
                     if (!Enum.TryParse<OfferResult>(parts[2], out offerResult))
                     {
                         OutputInvalidArgument(command, parts[2]);
+                        return true;
                     }
                     offerSystem.Offer(shipmentId, offerResult, GetDriverName(parts, 3));
                     break;
@@ -83,12 +108,17 @@ namespace ConvoyOfferSystem
 
         private static void OutputUnrecognizedCommand(string command)
         {
-            Console.WriteLine(string.Format("Unrecognized command \'{0}\'", command));
+            Console.WriteLine(string.Format("Error: Unrecognized command \'{0}\'", command));
         }
 
         private static void OutputInvalidArgument(string command, string arg)
         {
-            Console.WriteLine(string.Format("Invalid argument \'{0}\' for command \'{1}\'", arg, command));
+            Console.WriteLine(string.Format("Error: Invalid argument \'{0}\' for command \'{1}\'", arg, command));
+        }
+
+        private static void OutputInsufficientArguments(string command)
+        {
+            Console.WriteLine(string.Format("Error: Insufficient arguments provided for command \'{0}\'", command));
         }
     }
 }
--- ConvoyOfferSystem/ConvoyOfferSystem/ConsoleOutputOfferSystem.cs
+++ ConvoyOfferSystem/ConvoyOfferSystem/ConsoleOutputOfferSystem.cs
@@ -8,23 +8,32 @@ namespace ConvoyOfferSystem
 {
     public class ConsoleOutputOfferSystem : IOfferSystem
     {
-        SortedSet<Driver> _drivers;
+        private const string _noValidDriversString = "NOBODY";
+        SortedSet<Driver> _availableDrivers;
+        Dictionary<string, Driver> _nameToDriver;
+        Dictionary<int, int> _shipmentToCapacity;
 
         public ConsoleOutputOfferSystem()
         {
-            _drivers = new SortedSet<ConvoyOfferSystem.Driver>();
+            _availableDrivers = new SortedSet<Driver>();
+            _nameToDriver = new Dictionary<string, Driver>();
+            _shipmentToCapacity = new Dictionary<int, int>();
         }
 
         // Assume unique driver names?
         public void Driver(string driverName, int capacity)
         {
-            _drivers.Add(new Driver(driverName, capacity));
+            Driver d = new Driver(driverName, capacity);
+            _availableDrivers.Add(d);
+            _nameToDriver.Add(driverName, d);
         }
 
         public string Shipment(int shipmentId, int capacity)
         {
+            _shipmentToCapacity.Add(shipmentId, capacity);
+
             // or is it capacity - 1?
-            var candidates = _drivers.GetViewBetween(new Driver(string.Empty, capacity), _drivers.Max);
+            var candidates = _availableDrivers.GetViewBetween(new Driver(string.Empty, capacity), _availableDrivers.Max);
             return string.Empty;
 
             if (candidates.Count > 0)
@@ -32,12 +41,10 @@ namespace ConvoyOfferSystem
                 Driver bestCandidate = candidates.First();
                 foreach (var c in candidates)
                 {
-                    if (!c.RejectedExpiredOffers.Contains(shipmentId))
+                    if (!c.RejectedExpiredOffers.Contains(shipmentId) &&
+                        c.AcceptedOfferCount < bestCandidate.AcceptedOfferCount)
                     {
-                        if (c.AcceptedOfferCount < bestCandidate.AcceptedOfferCount)
-                        {
-                            bestCandidate = c;
-                        }
+                        bestCandidate = c;
                     }
                 }
 
@@ -45,13 +52,40 @@ namespace ConvoyOfferSystem
             }
             else
             {
-                return "nobody";
+                return _noValidDriversString;
             }
         }
 
         public string Offer(int shipmentId, OfferResult result, string driverName)
         {
-            return string.Empty;
+            Driver driver;
+            if (!_nameToDriver.TryGetValue(driverName, out driver))
+            {
+                return string.Format("Error: unrecognized driver \'{0}\'", driverName);
+            }
+            int requiredCapacity;
+            if (!_shipmentToCapacity.TryGetValue(shipmentId, out requiredCapacity))
+            {
+                return string.Format("Error: unrecognized shipment ID \'{0}\'", shipmentId);
+            }
+
+            switch (result)
+            {
+                case OfferResult.Accept:
+                    // Driver accepted the job, remove from pool of available drivers
+                    _availableDrivers.Remove(driver);
+                    driver.IncrementAcceptedOfferCount();
+                    return null;
+
+                case OfferResult.Expire:
+                case OfferResult.Pass:
+                    // Offer expired or driver rejected, return next best driver
+                    driver.AddRejectedExpiredOffer(shipmentId);
+                    return Shipment(shipmentId, requiredCapacity);
+
+                default:
+                    return null;
+            }
         }
     }
 }
